ac7dede2265c837d8c66955ef95c93562ac0423a
zoro <s0585804335@gmail.com>
2026-01-28 12:31:43 +0200

    עודכן שלב 2: שגיאות

--- my_network_space.py
+++ my_network_space.py
@@ -1,12 +1,27 @@
-from lib_network_space import SpaceEntity,SpaceNetwork,Packet
+from lib_network_space import SpaceEntity, SpaceNetwork, Packet
+from lib_network_space import TemporalInterferenceError, DataCorruptedError
+import time
+
 
 class Satellite(SpaceEntity):
-
     def receive_signal(self, packet):
-        print (f"[{self.name}] Received: {packet}")
+        print(f"[{self.name}] Received: {packet}")
 
-my=SpaceNetwork(1)
-Sat1=Satellite("aba",100)
-Sat2=Satellite("ema",200)
-sos=Packet("אנחנו בחיים ואתם?",Sat1,Sat2)
-my.send(sos)
+
+def transmission_attempt(packet,network):
+    while True:
+        try:
+            network.send(packet)
+            break
+        except TemporalInterferenceError:
+            print("waiting ,Interference...")
+            time.sleep(2)
+        except DataCorruptedError:
+            print("Data corrupted ,retrying...")
+
+
+network = SpaceNetwork(2)
+Sat1 = Satellite("aba", 100)
+Sat2 = Satellite("ema", 200)
+packet = Packet("אנחנו בחיים ואתם?", Sat1, Sat2)
+transmission_attempt(packet,network)
